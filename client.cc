65cb3667f8247016b7eeb81289402d628aeb92a7
Michal Izworski <m.izworski7@gmail.com>
2017-05-08 18:00:56 +0200

    Cleanup client.

--- client.cc
+++ client.cc
@@ -7,6 +7,12 @@
 const int FAILURE = 1;
 const int MAX_UDP_PACKET_SIZE = 65507;
 
+struct message {
+    uint64_t timestamp;
+    char ch;
+    std::string file;
+};
+
 void init_addr_hints(struct addrinfo &addr_hints) {
     memset(&addr_hints, 0, sizeof(struct addrinfo));
 
@@ -21,18 +27,12 @@ void init_addr_hints(struct addrinfo &addr_hints) {
     addr_hints.ai_next = NULL;
 }
 
-
 std::vector<unsigned char> intToBytes(uint64_t paramInt) {
     std::vector<unsigned char> arrayOfByte(8);
     for (int i = 0; i < 8; i++)
         arrayOfByte[7 - i] = (unsigned char) (paramInt >> (i * 8));
     return arrayOfByte;
 }
-
-struct message {
-    uint64_t timestamp;
-    char ch;
-};
 
 void parse_message(std::string timestamp_str, const char msg_char, message &message) {
     uint64_t timestamp = std::stoull(timestamp_str);
@@ -46,6 +46,52 @@ void parse_message(std::string timestamp_str, const char msg_char, message &mess
 
     message.timestamp = htobe64(timestamp);
     message.ch = msg_char;
+}
+
+void initialize_connection(addrinfo &addr_hints, addrinfo *&addr_result, const std::string &host_str,
+                           const std::string &port_str, sockaddr_in &my_address, int32_t &sock) {
+    if (getaddrinfo(host_str.c_str(), NULL, &addr_hints, &addr_result) != 0) {
+        throw std::runtime_error("Failed to get address of server.");
+    }
+
+    my_address.sin_family = AF_INET; // IPv4
+    my_address.sin_addr.s_addr = ((struct sockaddr_in *) (addr_result->ai_addr))->sin_addr.s_addr;
+    my_address.sin_port = htons((uint16_t) stoi(port_str));
+
+    freeaddrinfo(addr_result);
+
+    /// Open socket
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        throw std::runtime_error("Failed to open socket.");
+    }
+}
+
+std::string &serialize_message(const message &msg, std::string &message) {
+    std::ostringstream os;
+    auto msg_timestamp_bytes = intToBytes(msg.timestamp);
+    for (auto byte : msg_timestamp_bytes) {
+        os << byte;
+    }
+    os << msg.ch;
+    message = os.str();
+    return message;
+}
+
+message &deserialize_message(const char *raw_msg, ssize_t rcv_len, message &msg) {
+    msg.timestamp = 0;
+    int32_t mult = 1;
+    for (int i = 0; i < 8; ++i) {
+            msg.timestamp += ((uint8_t) raw_msg[i]) * mult;
+            mult *= 256;
+        }
+    msg.ch = raw_msg[8];
+
+
+    size_t file_beg = 9;
+    std::__cxx11::string file_content(raw_msg, rcv_len);
+    msg.file = file_content.substr(file_beg);
+    return msg;
 }
 
 int main(int argc, char *argv[]) {
@@ -80,40 +126,23 @@ int main(int argc, char *argv[]) {
     /// Host and port
     std::string host_str(argv[3]);
 
-    std::string port_str;
-
-    port_str = "20160";
+    std::string port_str("20160");
     if (argc == 5) {
         port_str = argv[4];
     }
 
-    if (getaddrinfo(host_str.c_str(), NULL, &addr_hints, &addr_result) != 0) {
-        std::cerr << "Failed to get address of server." << std::endl;
+    try {
+        initialize_connection(addr_hints, addr_result, host_str, port_str, my_address, sock);
+    } catch (std::runtime_error e) {
+        std::cerr << e.what() << std::endl;
         return FAILURE;
     }
 
-    my_address.sin_family = AF_INET; // IPv4
-    my_address.sin_addr.s_addr = ((struct sockaddr_in *) (addr_result->ai_addr))->sin_addr.s_addr;
-    my_address.sin_port = htons((uint16_t) std::stoi(port_str));
+    std::string message;
 
-    freeaddrinfo(addr_result);
-
-    /// Open socket
-    sock = socket(PF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        std::cerr << "Failed to open socket." << std::endl;
-        return FAILURE;
-    }
-
-    std::ostringstream os;
-    auto msg_timestamp_bytes = intToBytes(msg.timestamp);
-    for (auto byte : msg_timestamp_bytes) {
-        os << byte;
-    }
-    os << msg_char;
-    std::string message(os.str());
-
+    message = serialize_message(msg, message);
     size_t msg_len = message.size();
+
     socklen_t rcv_address_len = (socklen_t) sizeof(my_address);
     ssize_t snd_len = sendto(sock, message.c_str(), msg_len, 0, (struct sockaddr *) &my_address, rcv_address_len);
 
@@ -126,28 +155,19 @@ int main(int argc, char *argv[]) {
     while (true) {
         struct sockaddr_in server_address;
         char raw_msg[MAX_UDP_PACKET_SIZE];
-        int flags = 0;
+
         size_t rcv_msg_len = MAX_UDP_PACKET_SIZE;
         rcv_address_len = (socklen_t) sizeof(server_address);
-        ssize_t rcv_len = recvfrom(sock, &raw_msg, rcv_msg_len, flags, (struct sockaddr *) &server_address,
+        ssize_t rcv_len = recvfrom(sock, &raw_msg, rcv_msg_len, 0, (struct sockaddr *) &server_address,
                                    &rcv_address_len);
-
-        struct message msg;
-        msg.timestamp = 0;
-        int32_t mult = 1;
-        for (int i = 0; i < 8; ++i) {
-            msg.timestamp += ((uint8_t) raw_msg[i]) * mult;
-            mult *= 256;
-        }
-        msg.ch = raw_msg[8];
 
         if (rcv_len < 0) {
             std::cerr << "read." << std::endl;
             return FAILURE;
         }
-        size_t file_beg = 9;
-        std::string file_content(raw_msg, rcv_len);
-        file_content = file_content.substr(file_beg);
-        std::cout << msg.timestamp << " " << msg.ch << " " << file_content << "\n" << std::flush;
+
+        msg = deserialize_message(raw_msg, rcv_len, msg);
+
+        std::cout << msg.timestamp << " " << msg.ch << " " << msg.file << "\n" << std::flush;
     }
 }
